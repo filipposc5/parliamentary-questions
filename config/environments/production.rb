9e778c245cb542082965eb185d42bd37966af855
Matt Peperell <mattp@automationlogic.com>
2016-06-06 15:32:14 +0100

    production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ ParliamentaryQuestions::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.enabled = true
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
